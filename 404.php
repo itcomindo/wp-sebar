41286cffc3c6e8f433a50b0a962a371156e81a18
Budi Haryono <mail.budiharyono@gmail.com>
2024-02-04 11:34:39 +0700

    Add "under construction" section to page templates

--- 404.php
+++ 404.php
@@ -5,3 +5,16 @@
  */
 
 defined('ABSPATH') or die('No script kiddies please!');
+
+
+get_header();
+
+?>
+<section>
+    <div class="container">
+        <h1>under construction</h1>
+    </div>
+</section>
+<?php
+
+get_footer();
--- search.php
+++ search.php
@@ -7,4 +7,13 @@
 defined('ABSPATH') or die('No script kiddies please!');
 
 get_header();
+
+?>
+<section>
+    <div class="container">
+        <h1>under construction</h1>
+    </div>
+</section>
+<?php
+
 get_footer();
