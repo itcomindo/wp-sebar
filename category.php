90fc9d70000bfa76a63388ddb478a29670d7edc4
Budi Haryono <mail.budiharyono@gmail.com>
2024-02-06 13:13:37 +0700

    Update new post query path in category.php

--- category.php
+++ category.php
@@ -7,5 +7,5 @@
 defined('ABSPATH') or die('No script kiddies please!');
 
 get_header();
-get_template_part('queries/new-post-query');
+get_template_part('inc/sections/new-post-section');
 get_footer();
